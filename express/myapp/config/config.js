24b9b2342b92ddd0da2a89530362299850b882fc
gabriel.novaes <gabriel.novaes@fs.com.br>
2019-11-06 16:30:35 -0300

    save leads collected on landing page

--- express/myapp/config/config.js
+++ express/myapp/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   "development": {
     "username": "express-mvp-dbuser",
     "password": '123.456',
-    "database": "express-mpv-db",
+    "database": "express-mvp-db",
     "host": "127.0.0.1",
     "dialect": "postgres",
     "port": 5432
